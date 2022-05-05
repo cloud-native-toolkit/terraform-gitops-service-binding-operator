041266fc4ef2593c04104ca8f2b715945a9169c7
tcskill <tskill@us.ibm.com>
2022-05-05 14:24:14 -0500

    adds sbo via helm deploy

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+CHARTNAME="$1"
+DEST_DIR="$2"
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/chart/${CHARTNAME}"; pwd -P)
 
-NAME="$1"
-DEST_DIR="$2"
+mkdir -p "${DEST_DIR}"
 
-## Add logic here to put the yaml resource content in DEST_DIR
-
-find "${DEST_DIR}" -name "*"
+## put the yaml resource content in DEST_DIR
+cp -R "${CHART_DIR}"/* "${DEST_DIR}"
--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name        = module.sbo_module.name
+        branch      = module.sbo_module.branch
+        namespace   = module.sbo_module.namespace
+        server_name = module.sbo_module.server_name
+        layer       = module.sbo_module.layer
+        layer_dir   = module.sbo_module.layer == "infrastructure" ? "1-infrastructure" : (module.sbo_module.layer == "services" ? "2-services" : "3-applications")
+        type        = module.sbo_module.type
       })
     }
   }
